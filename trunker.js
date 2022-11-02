1844040e93a693571a5b88b000fec35fa179acfa
Retrontology <retrontology@hotmail.com>
2022-11-01 23:25:46 -0500

    whoops

--- trunker.js
+++ trunker.js
@@ -8,7 +8,7 @@ class Trunker {
     }
 
     addChannel(channel) {
-        this.addGroup(channel.group);
+        this.addGroup(sanitizeName(channel.genre));
         this.channels[channel.server_name] = new Channel(channel);
     }
 
